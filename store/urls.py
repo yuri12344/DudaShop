bcb9b083ee3fa7a8cd8f72556cf30fe2b8c8b0c8
Yuri Caetano <yuuri.caetano@gmail.com>
2022-08-21 22:31:54 -0300

    feat: create the route to get all products from category

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,8 @@ from rest_framework.routers import SimpleRouter
 from .views import CategoryView, ProductView
 
 
-app_name = "store"
-
-
 router = SimpleRouter()
-router.register(r'category', CategoryView)
-router.register(r'product', ProductView)
 
-urlpatterns = router.urls
+router.register('category', CategoryView)
+router.register('product', ProductView)
+
--- store/views.py
+++ store/views.py
@@ -1,13 +1,23 @@
+from unicodedata import category
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
+    @action(detail=True, methods=['GET'])
+    def products(self, request, pk=None):
+        category = self.get_object()
+        serializer = CategorySerializer(category.products.all(), many=True)
+        return Response(serializer.data)
+
+
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
